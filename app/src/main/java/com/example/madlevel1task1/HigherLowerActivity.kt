93b4429532d39bbc371e0612465cf4db352450fb
dylan2050 <dyhayg@gmail.com>
2020-09-06 16:51:50 +0200

    Added functionality to the app

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -2,10 +2,92 @@ package com.example.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    /**
+     * Set the initial (UI) state of the game.
+     */
+    private fun initViews() {
+        updateUI()
+
+        binding.btnHigher.setOnClickListener { onHigherClick() }
+        binding.btnEqual.setOnClickListener { onEqualClick() }
+        binding.btnLower.setOnClickListener { onLowerClick() }
+    }
+
+    /**
+     * Update the last throw text and the dice image resource drawable with the current throw.
+     */
+    private fun updateUI() {
+        binding.tvLastThrowValue.text = lastThrow.toString()
+
+        when (currentThrow) {
+            1 -> binding.ivDice.setImageResource(R.drawable.dice1)
+            2 -> binding.ivDice.setImageResource(R.drawable.dice2)
+            3 -> binding.ivDice.setImageResource(R.drawable.dice3)
+            4 -> binding.ivDice.setImageResource(R.drawable.dice4)
+            5 -> binding.ivDice.setImageResource(R.drawable.dice5)
+            6 -> binding.ivDice.setImageResource(R.drawable.dice6)
+        }
+    }
+
+    /**
+     * Rolls the dice
+     */
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUI()
+    }
+
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+    }
+
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+    }
+
+    private fun onHigherClick() {
+        rollDice()
+
+        if (currentThrow > lastThrow) {
+            onAnswerCorrect()
+        } else {
+            onAnswerIncorrect()
+        }
+    }
+
+    private fun onLowerClick() {
+        rollDice()
+
+        if (currentThrow < lastThrow) {
+            onAnswerCorrect()
+        } else {
+            onAnswerIncorrect()
+        }
+    }
+
+    private fun onEqualClick() {
+        rollDice()
+
+        if (currentThrow == lastThrow) {
+            onAnswerCorrect()
+        } else {
+            onAnswerIncorrect()
+        }
     }
 }
